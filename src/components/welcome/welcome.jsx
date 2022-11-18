42d377372486df06d01ecbaeb17b756eedda150f
lewispotticary <lewispotticary@gmail.com>
2022-11-18 12:55:40 +0000

    Added styling to welcome.jsx

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-import { Typography } from '@mui/material'
+import { Typography, Button } from '@mui/material';
+
+import styles from './welcome.module.css';
 
 function welcome() {
   return (
-    <div>
+    <div className={styles.welcomeContainer}>
       <Typography fontFamily={'Trebuchet MS'} variant='h2'>Hi.</Typography>
-      <Typography fontFamily={'Courier New'} variant='h2'>Welcome to Simple CV Creator</Typography>
+      <Typography fontFamily={'Courier New'} variant='h2'>Welcome to Simple CV Creator.</Typography>
+      <div className={styles.startContainer}>
+        <Button style={{ width: 500}} variant="contained">Start</Button>
+      </div>
     </div>
   )
 }
