70b944c40ec596bc3b9e78d2fe36a64bfd8ff32b
lewispotticary <lewispotticary@gmail.com>
2023-01-03 19:33:34 +0000

    Added browser router to welcome component

--- src/components/welcome/welcome.jsx
+++ src/components/welcome/welcome.jsx
@@ -6,23 +6,29 @@ import styles from './welcome.module.css';
 
 import { motion } from 'framer-motion';
 
+import { BrowserRouter } from 'react-router-dom';
+
+import { HashLink } from 'react-router-hash-link';
+
 function welcome({displayMode}) {
 
   console.log("#" + displayMode.backgroundColour)
 
   return (
-    <div className={styles.welcomeContainer}>
-      <div style={{backgroundColor: "#" + displayMode.backgroundColour, color: "#" + displayMode.fontColour}}>
-          <motion.div animate={{ y: [1800, 100, 0] }} transition={{ ease: "easeOut", duration: 1, delay: 0 }}>
-              <Typography fontFamily={'Courier New'} variant='h2'><b>Welcome </b>to Simple CV Creator.</Typography>
-          </motion.div>
+    <BrowserRouter>
+      <div className={styles.welcomeContainer}>
+        <div style={{backgroundColor: "#" + displayMode.backgroundColour, color: "#" + displayMode.fontColour}}>
+            <motion.div animate={{ y: [1800, 100, 0]}} transition={{ ease: "easeOut", duration: 1, delay: 0 }}>
+                <Typography fontFamily={'Courier New'} variant='h2'><b>Welcome </b>to Simple CV Creator.</Typography>
+            </motion.div>
+        </div>
+        <motion.div animate={{ x: [1000, 100, 0] }} transition={{ ease: "easeOut", duration: 1, delay: 2 }}>
+            <div className={styles.startContainer}>
+                <HashLink style={{ textDecoration: 'none' }} smooth className="navLinks" to="#Profile"><Button style={{ width: 500}} variant="contained">Create a CV</Button></HashLink>
+            </div>
+        </motion.div>
       </div>
-      <motion.div animate={{ x: [1000, 100, 0] }} transition={{ ease: "easeOut", duration: 1, delay: 2 }}>
-          <div className={styles.startContainer}>
-              <Button style={{ width: 500}} variant="contained">Create a CV</Button>
-          </div>
-      </motion.div>
-    </div>
+    </BrowserRouter>
   )
 }
 
